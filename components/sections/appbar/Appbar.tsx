0f71c138f72238b02ff679736c5c7347d85cbca4
waleed2000x <waleedahmed2000x@gmail.com>
2024-10-04 16:35:35 +0500

    Appbar testing

--- components/sections/appbar/Appbar.tsx
+++ components/sections/appbar/Appbar.tsx
@@ -1,9 +1,37 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link";
 
 export default function Appbar() {
   return (
     <div className="appbar">
-      <p>Start2 Finish Printing</p>
+      <h2>Start2 Finish Printing</h2>
+      <div className="nav-items">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <p>Open</p>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56">
+            {/* <DropdownMenuLabel>Panel Position</DropdownMenuLabel> */}
+            {/* <DropdownMenuSeparator /> */}
+            <div className="flex flex-col ml-[20px]">
+              <Link href="/">Top</Link>
+              <DropdownMenuSeparator />
+              <Link href="/">Bottom</Link>
+              <DropdownMenuSeparator />
+              <Link href="/">Right</Link>
+            </div>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </div>
   );
 }
