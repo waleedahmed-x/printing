f223217d7240370fad0227993a2d4393d2580982
waleed2000x <waleedahmed2000x@gmail.com>
2024-10-11 12:09:28 +0500

    Menu-Items Added | Menu-Items Styles Fixed | Few Icons Added

--- components/sections/appbar/Appbar.tsx
+++ components/sections/appbar/Appbar.tsx
@@ -1,37 +1,59 @@
 "use client";
 import React from "react";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import Link from "next/link";
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+import ChevronDown, { Hamburger } from "@/components/icons/Icons";
 
 export default function Appbar() {
   return (
     <div className="appbar">
+      <Hamburger />
       <h2>Start2 Finish Printing</h2>
       <div className="nav-items">
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <p>Open</p>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-56">
-            <DropdownMenuLabel>Panel Position</DropdownMenuLabel>
-            <div className="flex flex-col ml-[20px]">
-              <DropdownMenuLabel>
-                <Link href="/">Top</Link>
-              </DropdownMenuLabel>
-              <DropdownMenuLabel>
-                <Link href="/">Bottom</Link>
-              </DropdownMenuLabel>
-              <DropdownMenuLabel>
-                <Link href="/">Right</Link>
-              </DropdownMenuLabel>
-            </div>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                  <li className="row-span-3">
+                    <NavigationMenuLink asChild>
+                      <a
+                        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                        href="/"
+                      >
+                        <ChevronDown />
+                        <div className="mb-2 mt-4 text-lg font-medium">
+                          shadcn/ui
+                        </div>
+                        <p className="text-sm leading-tight text-muted-foreground">
+                          Beautifully designed components that you can copy and
+                          paste into your apps. Accessible. Customizable. Open
+                          Source.
+                        </p>
+                      </a>
+                    </NavigationMenuLink>
+                  </li>
+                  <p title="Introduction">
+                    Re-usable components built using Radix UI and Tailwind CSS.
+                  </p>
+                  <a href="/docs/installation" title="Installation">
+                    How to install dependencies and structure your app.
+                  </a>
+                  <a href="/docs/primitives/typography" title="Typography">
+                    Styles for headings, paragraphs, lists...etc
+                  </a>
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
       </div>
     </div>
   );
